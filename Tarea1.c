64da81df87fe0b418e8caca3dc4e28c9f2b267c5
Karen Peralta <56215336+Kaliss27@users.noreply.github.com>
2020-08-25 20:20:46 -0500

    Update Tarea1.c
    -Se agrego otra estructura y funciones para imprimir una lista de listas

--- Tarea1.c
+++ Tarea1.c
@@ -1,64 +1,58 @@
-////////////////////////////////TAREA 1
+//*Tarea 1. *//
 #include <stdio.h>
 #include <stdlib.h>
-//#include <archivo.h>
-#define l 3
+
+typedef struct elemento_lista
+{
+	int dato;
+	struct elemento_lista2 *sig;
+}e_lista;
 
 typedef struct elemento_lista1
 {
-	int dato;
+	int id;
+	e_lista2 *lista_datos;
 	struct elemento_lista1 *sig;
 }e_lista1;
 
-/*typedef struct elemento_lista0
-{
-	int id;
-	e_lista1 *elemento;
-	struct elemento_lista0 *sig;
-}e_lista0;*/
 /*------------------Funciones-------------------*/
-int insertar_elemento_ent(e_lista1 **inicio,int d);
-void ver_lista_ent(e_lista1 *inicio);
+
+int insertar_elemento_ent(e_lista **inicio,int d);
+int insertar_elemento_lista(e_lista1 **inicio,int idl);
+void ver_listas(e_lista1 *inicio);
+void ver_elementos(e_lista *inicio);
 int eliminar_elemento_l(e_lista1 **inicio,int d,int bnd);
+void menu();
 
 int main(int argc, char const *argv[])
 {
     e_lista1 *inicio_l=NULL;
-	int d,bnd,dd;
+	int d,bnd,dd,opc;
 	do{
-		printf("Ingrese un n%cmero entero:",163);
-		scanf("%i",&d);
-		dd=insertar_elemento_ent(&inicio_l,d);
-		/*if(dd==1)
-			printf("Se agreg%c correctamente\n",162);*/
-		if(dd==0)
-			printf("No se agreg%c correctamente\n",162);
-		ver_lista_ent(inicio_l);
-		printf("1->Otro\n0->Fin\t:");
-		scanf("%i",&bnd);
+		menu();
+		scanf("%i",&opc);
+		switch(opc){
+			case 1:
+			    
+			break;
+		}
+
 	}while(bnd==1);
 	ver_lista_ent(inicio_l);
 	printf("\n");
 	do{
-		printf("Ingrese un n%cmero entero a eliminar:",163);
-		scanf("%i",&d);
-		dd=eliminar_elemento_l(&inicio_l,d,0);
-		if(dd==1)
-			printf("Se elimin%c correctamente\n",162);
-		if(dd==0)
-			printf("No se elimin%c correctamente\n",162);
-		ver_lista_ent(inicio_l);
-		printf("\n");
-		printf("1->Otro\n0->Fin\t:");
-		scanf("%i",&bnd);
+		
 	}while(bnd==1);
 	return 0;
 }
 
 /*------------------Cuerpo de Funciones-------------------*/
-int insertar_elemento_ent(e_lista1 **inicio,int d)
+void menu(){
+	printf("===MENU DE OPCIONES===\n1->Insertar nuevo elemento a una lista\n2->Eliminar elemento de una lista\n3->Ver lista\n:");
+}
+int insertar_elemento_ent(e_lista **inicio,int d)
 {
-	e_lista1 *newe=(e_lista1*)malloc(sizeof(e_lista1));
+	e_lista *newe=(e_lista*)malloc(sizeof(e_lista));
 	if(!newe)
 		return 0;
 	newe->dato=d;
@@ -72,12 +66,23 @@ int insertar_elemento_ent(e_lista1 **inicio,int d)
 	return 1;
 }
 
-void ver_lista_ent(e_lista1 *inicio)
+void ver_elementos(e_lista *inicio)
 {
 	if(inicio!=NULL)
 	{
+		if()
 		printf("%i,", inicio->dato);
 		ver_lista_ent(inicio->sig);
+	}
+	return;
+}
+
+void ver_listas(e_lista1 *inicio)
+{
+	if(inicio!=NULL)
+	{
+		ver_elementos(inicio->lista_datos);
+		ver_listas(inicio->sig);
 	}
 	return;
 }

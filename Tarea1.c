505223187bc3c0b2597cf108b8bbfd98ff0b4f86
Karen Peralta <56215336+Kaliss27@users.noreply.github.com>
2020-08-26 21:58:49 -0500

    Update Tarea1.c
    -Correcta funciòn de impresiòn
    -correcto almacenamiento de primer dato cuando no existe lista

--- Tarea1.c
+++ Tarea1.c
@@ -5,20 +5,20 @@
 typedef struct elemento_lista
 {
 	int dato;
-	struct elemento_lista2 *sig;
+	struct elemento_lista *sig;
 }e_lista;
 
 typedef struct elemento_lista1
 {
 	int id;
-	e_lista2 *lista_datos;
+	e_lista *lista_datos;
 	struct elemento_lista1 *sig;
 }e_lista1;
 
 /*------------------Funciones-------------------*/
-
+e_lista1* buscar_lista(e_lista1 *actual,int d);
 int insertar_elemento_ent(e_lista **inicio,int d);
-int insertar_elemento_lista(e_lista1 **inicio,int idl);
+int insertar_elemento_lista(e_lista1 **inicio,int idl,int num);
 void ver_listas(e_lista1 *inicio);
 void ver_elementos(e_lista *inicio);
 int eliminar_elemento_l(e_lista1 **inicio,int d,int bnd);
@@ -33,12 +33,15 @@ int main(int argc, char const *argv[])
 		scanf("%i",&opc);
 		switch(opc){
 			case 1:
-			    
+			printf("Ingrese un numero:\n");
+			scanf("%i",&d);
+			insertar_elemento_lista(&inicio_l,0,d);
+			printf("------\n");
+			ver_listas(inicio_l);			    
 			break;
 		}
 
 	}while(bnd==1);
-	ver_lista_ent(inicio_l);
 	printf("\n");
 	do{
 		
@@ -47,6 +50,15 @@ int main(int argc, char const *argv[])
 }
 
 /*------------------Cuerpo de Funciones-------------------*/
+e_lista1* buscar_lista(e_lista1 *actual,int d){
+	if(actual && actual->id==d)
+		return actual;
+	if(!actual)
+		return NULL;
+	buscar_lista(actual->sig,d);
+	
+}
+
 void menu(){
 	printf("===MENU DE OPCIONES===\n1->Insertar nuevo elemento a una lista\n2->Eliminar elemento de una lista\n3->Ver lista\n:");
 }
@@ -56,47 +68,61 @@ int insertar_elemento_ent(e_lista **inicio,int d)
 	if(!newe)
 		return 0;
 	newe->dato=d;
-	if((*inicio)==NULL)
-	{
-			newe->sig=NULL;
-	}else{
-	        newe->sig=(*inicio);
-	}
+    newe->sig=(*inicio);
+    //printf("%p\n",newe->sig);
 	(*inicio)=newe;
+	//printf("%p\n",(*inicio));
 	return 1;
 }
-
+int insertar_elemento_lista(e_lista1 **inicio,int idl,int num){
+	if((*inicio)){
+		e_lista1 *g_lista=buscar_lista((*inicio),idl);
+		insertar_elemento_ent(&g_lista->lista_datos,num);
+		return 1;
+	}else{
+		e_lista1 *newe=(e_lista1*)malloc(sizeof(e_lista1));
+		if(!newe)
+			return 0;
+		//printf("%p\n",(*inicio));
+		newe->lista_datos=NULL;
+		//printf("%p\n",newe->lista_datos);
+		insertar_elemento_ent(&newe->lista_datos,num);
+		newe->sig=NULL;
+		//printf("%p\n",newe->lista_datos);
+		//printf("%p\n",newe->sig);
+		(*inicio)=newe;
+		//printf("%p\n",(*inicio));
+		return 1;
+	}
+}
+/*Imprimir lista de listas*/
 void ver_elementos(e_lista *inicio)
 {
-	if(inicio!=NULL)
+	if(inicio)
 	{
-		if()
 		printf("%i,", inicio->dato);
-		ver_lista_ent(inicio->sig);
+		//printf("%p\n",inicio->sig);
+		ver_elementos(inicio->sig);
+	}else{
+		return;
 	}
-	return;
 }
 
 void ver_listas(e_lista1 *inicio)
 {
-	if(inicio!=NULL)
+	if(inicio)
 	{
+		//printf("%p\n",inicio);
+		//printf("%p\n",inicio->lista_datos);
 		ver_elementos(inicio->lista_datos);
+		//printf("%p\n",inicio->sig);
 		ver_listas(inicio->sig);
+	}else{
+		return;
 	}
-	return;
 }
-
-e_lista1* buscar_lista(e_lista1 *inicio_l,int d)
-{
-	if(inicio_l==NULL)
-		return NULL;
-	if(inicio_l->dato==d)
-		return inicio_l;
-	return buscar_lista(inicio_l->sig,d);
-}
-
-int eliminar_elemento_l(e_lista1 **inicio_l, int d,int bnd){
+//------------------
+/*int eliminar_elemento_l(e_lista1 **inicio_l, int d,int bnd){
 
 	if((*inicio_l) == NULL){
 		if(bnd==0)
@@ -114,6 +140,6 @@ int eliminar_elemento_l(e_lista1 **inicio_l, int d,int bnd){
     //return eliminar_elemento_l(&(*inicio_l)->sig,d,bnd);
 }
 return eliminar_elemento_l(&(*inicio_l)->sig,d,bnd);
-}
+}*/
 
 

2712e09b265defb6b9ffc10979f93659d6807fa7
Karen Peralta <56215336+Kaliss27@users.noreply.github.com>
2020-09-19 17:11:56 -0500

    Update archivo.h
    -Listas Simples
    -Pilas

--- archivo.h
+++ archivo.h
@@ -1,141 +1,101 @@
-//Biblioteca---Peralta Luna Karen Lisseth
+/*TAREA 2
+Biblioteca---Peralta Luna Karen Lisseth*/
 #include <stdlib.h>
 #include<stdio.h>
-//Estructuras
-typedef struct elemento{
-	int d;
-	struct elemento *sig;
-}e_gral;
-
-typedef struct elemento_Cola
+//ESTRUCTURAS
+typedef struct elemento_lista1     /*ELEMENTO LISTA SIMPLE*/
 {
-	e_gral *fre;
-	e_gral *fin;
-}ptrs_cola;
+	int dato;
+	struct elemento_lista1 *sig;
+}e_lista1;
 
-//Funciones listas
-int insertar_elemento_ent(e_gral **inicio,int d)
+typedef struct pila{              /*ELEMENTO PILA*/
+	int dato;
+	struct pila *sig;
+}e_Pila;
+/*
+  FUNCIONES PARA LISTAS, PILAS Y COLAS
+*/
+//FUNCIONES LISTAS SIMPLES
+int agregar_inicio(e_lista1 **inicio,int num)
 {
-	e_gral *newe=(e_gral*)malloc(sizeof(e_gral));
+	e_lista1 *newe=(e_lista1*)malloc(sizeof(e_lista1));
 	if(!newe)
 		return 0;
-	newe->d=d;
-	if((*inicio)==NULL)
-	{
-			newe->sig=NULL;
-	}else{
-	        newe->sig=(*inicio);
-	}
+	newe->dato=num;
+    newe->sig=(*inicio);
 	(*inicio)=newe;
 	return 1;
 }
-
-void ver_lista_ent(e_gral *inicio)
+void ver_lista(e_lista1 *inicio)
 {
-	if(inicio!=NULL)
-	{
-		printf("%i,", inicio->d);
-		ver_lista_ent(inicio->sig);
+	if(inicio){
+		printf("|%i|",inicio->dato);
+		ver_lista(inicio->sig);
 	}
 	return;
 }
-
-
-int eliminar_elemento_l(e_gral **inicio_l, int d,int bnd){
-
+e_lista1* buscar_elemento_x(e_lista1 *actual,int d){
+	if(actual && actual->dato==d)
+		return actual;
+	if(!actual)
+		return NULL;
+	buscar_elemento_x(actual->sig,d);
+	
+}
+int eliminar_elemento_x(e_lista1 **inicio_l,int d,int bnd){
 	if((*inicio_l) == NULL){
 		if(bnd==0)
 			return 0;
 		return 1;
 	}
-
-	if ((*inicio_l)->d == d){
-		e_gral *aux = (*inicio_l);
+	if ((*inicio_l)->dato == d){
+		e_lista1 *aux = (*inicio_l);
 		(*inicio_l) = (*inicio_l)->sig;
 		free(aux); 
 		bnd=1;
 		if((*inicio_l) == NULL){
 			return 1;
 		}
-	}
-	return eliminar_elemento_l(&(*inicio_l)->sig,d,bnd);
+    return eliminar_elemento_x(&(*inicio_l)->sig,d,bnd);
 }
-
-
-////Funciones colas
-int insertar_elemento_c(ptrs_cola **ptrs){
-	e_gral *newe=(e_gral*)malloc(sizeof(e_gral));
-	if(!newe)
-		return 0;
-	printf("Ingrese un n%cmero entero:",163);
-	scanf("%i",&newe->d);
-	newe->sig=NULL;
-	if((*ptrs)->fre==NULL)
-		{
-			(*ptrs)->fre=newe;
-
-		}else{
-			(*ptrs)->fin->sig=newe;
-		}	
-	(*ptrs)->fin=newe;
-	return 1;
+return eliminar_elemento_x(&(*inicio_l)->sig,d,bnd);
 }
-
-int buscar_elemento_c(ptrs_cola *ver,int d){
-	if(ver->fre!=NULL){
-		if(d==ver->fre->d)
-			return 1;
-		buscar_elemento_l(ver->fre->sig,d);
-    }
-    return 0;
-}
-
-void eliminacion(ptrs_cola **ptrs, int d,e_gral **act, e_gral **ant){
-	if ((*act) == NULL)
-		return;
-	
-	if ((*act)->d == d){
-
-		if ((*ptrs)->fre == (*ptrs)->fin){
-			(*ptrs)->fre = NULL;
-			(*ptrs)->fin = NULL;
-			free(ant);
-			return;
-		}
-
-		else if ((*act) == (*ptrs)->fre)
-			(*ptrs)->fre = (*ptrs)->fre->sig;
-
-		else if ((*act) == (*ptrs)->fin){
-			(*ant)->sig = NULL;
-			(*ptrs)->fin = (*ant);
-		}
-
-		else
-			(*ant)->sig = (*act)->sig;
-		free(ant);
-		return;
-	}
-
-	(*ant) = (*act);
-	(*act) = (*act)->sig;
-	eliminacion(&(*ptrs),d, &(*act), &(*ant));
-}
-
-//funciones pila
-e_gral* pop(e_gral **tope)
+//FUNCIONES PILAS
+int pila_vacia(e_Pila *pila)
 {
-	e_gral *aux=(*tope);
+	if(!pila)
+		return 1;
+	return 0;	 
+}
+e_Pila *crear_ePila()
+{
+	e_Pila *newe=(e_Pila*)malloc(sizeof(e_Pila));
+	if(!newe)
+		return NULL;
+	return newe;
+
+}
+void push(e_Pila **pila,int d)
+{
+	e_Pila *newe=crear_ePila();
+	newe->dato=d;
+	newe->sig=(*pila);
+	(*pila)=newe;
+	return;
+}
+e_Pila *pop(e_Pila **tope)
+{
+	e_Pila *aux=(*tope);
 	(*tope)=(*tope)->sig;
 	return aux;
 }
-
-int push(int d,e_gral **tope)
+void ver_pila(e_Pila *tope)
 {
-	e_gral *newe=(e_gral*)malloc(sizeof(e_gral));
-	if(!newe)
-		return 0;
-	newe->d=d;
-	newe->sig=(*tope);
-	(*tope)=newe;
+	if(tope)
+	{
+		printf("%i\n",tope->dato);
+		ver_pila(tope->sig);
+	}
+	return;
 }

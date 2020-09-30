fce153f7dfb6d7a481d0775cc847b5e04eff45fe
Karen Peralta <56215336+Kaliss27@users.noreply.github.com>
2020-09-29 19:05:42 -0500

    Update T3.c

--- T3.c
+++ T3.c
@@ -1,69 +1,98 @@
-//Tarea 3-Peralta Luna Karen Lisseth
+/*Tarea 3
+-Peralta Luna Karen Lisseth
+-S17002346
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct nodoBinario
+typedef struct nodo_padre
 {
 	int dato;
-	struct nodoBinario *izq;
-	struct nodoBinario *der;	
-}nodoB;
+	struct punteros_hijos *hijos;
+}n_padre;
 
-int insertar(nodoB **axraiz,int d);
-nodoB* crearNodo(int d);
-void ver_Arbol(nodoB *raiz,int c,int i);
+typedef struct punteros_hijos
+{
+	n_padre * raiz;
+	struct punteros_hijos *hermano;
+}hijo;
+typedef struct cola_hijos
+{
+	hijo *frente;
+	hijo *final;
+}c_hijos;
+/*--------------------------------------*/
+int crear_cola(c_hijos **cola,n_padre *nhijo);
+n_padre *crear_nodo_p(int d);
+int insertar_nuevo_nodo(n_padre **raiz,int d,int p);
+void imprimir_hojas(hijo *frente);
+void imprimir_arbol(n_padre *raiz);
 
 int main(int argc, char const *argv[])
 {
-	nodoB *raiz=NULL;
-	int d,bnd;
-	do{
-		printf("Ingrese un n%cmero entero:",163);
-		scanf("%i",&d);
-		insertar(&raiz,d);
-		printf("1->Otro n%cmero\n0->Terminar\t:",163);
-		scanf("%i",&bnd);
-	}while(bnd==1);
-	printf("====Arbol binario====\n");
-	ver_Arbol(raiz,0,0);
+	n_padre *raiz=NULL;
+	insertar_nuevo_nodo(&raiz,65,0);
+	printf("%c\n",raiz->dato);
+	printf("insertar_nuevo_nodo\n");
+	insertar_nuevo_nodo(&raiz,66,65);
+	printf("desp\n");
+	imprimir_arbol(raiz);
 	return 0;
 }
-nodoB* crearNodo(int d){
-	nodoB *newe=(nodoB*)malloc(sizeof(nodoB));
-	newe->dato=d;
-	newe->der=NULL;
-    newe->izq=NULL;		
-	return newe;	
+int crear_cola(c_hijos **cola,n_padre *nhijo)
+{
+	hijo *neweh=(hijo*) malloc(sizeof(hijo));
+	if(!neweh)
+		return 0;
+	neweh->raiz=nhijo;
+	neweh->hermano=NULL;
+	(*cola)->frente=neweh;
+	(*cola)->final=neweh;
+	return 1;
 }
-///////////INSERTAR NODO A ARBOL BINARIO////////////////////////
-int insertar(nodoB **axraiz,int d){
-	if((*axraiz)==NULL){
-		nodoB *nnuevo=crearNodo(d);
-		if(!nnuevo){
-			return 0;
-		}
-		(*axraiz)=nnuevo;
+n_padre *crear_nodo_p(int d)
+{
+	n_padre *newe=(n_padre*)malloc(sizeof(n_padre));
+	if(!newe)
+		return NULL;
+	newe->dato=d;
+	newe->hijos=NULL;
+	return newe;
+}
+int insertar_nuevo_nodo(n_padre **raiz,int d,int p)
+{
+	if(!(*raiz)){
+		(*raiz)=crear_nodo_p(d);
 		return 1;
 	}
-	if(d>(*axraiz)->dato){
-		return insertar(&(*axraiz)->der,d);
-	}else{
-		if((*axraiz)->dato==d) return 2;
-		return insertar(&(*axraiz)->izq,d);
-	}	
-}
-void ver_Arbol(nodoB *raiz,int c,int i){
-	if(raiz==NULL){
-		return;
+	if((*raiz)->dato==p)
+	{
+		n_padre *aux=crear_nodo_p(d);
+		c_hijos *p;
+		crear_cola(&p,aux);
+		(*raiz)->hijos=p->final;
+		//printf("inserto :%i\n",aux->dato);
+		return 1;
 	}
-	else{
-		ver_Arbol(raiz->der,c+1,0);
-		for(i=0;i<c;i++){
-			printf("   ");
-		}
-		printf("%i\n",raiz->dato);
-		ver_Arbol(raiz->izq,c+1,0);
-	}
-	
 }
+void imprimir_hojas(hijo *frente)
+{
+	if(frente)
+	{
+		imprimir_arbol(frente->raiz);
+		imprimir_hojas(frente->hermano);
+	}
+	return;
 
+}
+void imprimir_arbol(n_padre *raiz)
+{
+	printf("iarbol\n");
+	if(raiz)
+	{
+		printf("Padre %p\n Hijo:%p\n",raiz,raiz->hijos);
+		printf("Padre:%c",raiz->dato);
+		imprimir_hojas(raiz->hijos);
+	}
+	return;
+}

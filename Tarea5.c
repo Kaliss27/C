49432bff33547aa041981b60cb9b53bd2b51fc90
Karen Peralta <56215336+Kaliss27@users.noreply.github.com>
2020-10-13 00:12:18 -0500

    Update Tarea5.c
    Balanceo correcto al insertar

--- Tarea5.c
+++ Tarea5.c
@@ -13,7 +13,8 @@ typedef struct nodoBinario
 	struct nodoBinario *der;	
 }nodoB;
 
-int insertar(nodoB **axraiz,int d);
+//int insertar(nodoB **axraiz,int d);
+int insertar_balanceado(nodoB **axraiz,int d);
 nodoB* crearNodo(int d);
 void ver_Arbol(nodoB *raiz,int c,int i);
 int borrar_dato(nodoB **raiz,int d);
@@ -22,7 +23,10 @@ int buscar_dato(nodoB *raiz,int d);
 void contar_hojas(nodoB *raiz,int *hojas);
 int altura_arbol(nodoB *raiz);
 int equilibrio_arbol(nodoB *raiz);
-
+int balanceo_DD(nodoB **raiz);
+int balanceo_II(nodoB **raiz);
+int balanceo_DI(nodoB **raiz);
+int balanceo_ID(nodoB **raiz);
 
 int main(int argc, char const *argv[])
 {
@@ -68,7 +72,7 @@ int main(int argc, char const *argv[])
 		}	
 		system("clear");
 	}while(opc!=6);*/
-	insertar(&raiz,120);
+	/*insertar(&raiz,120);
 	insertar(&raiz,87);
 	insertar(&raiz,43);
 	insertar(&raiz,65);
@@ -78,7 +82,7 @@ int main(int argc, char const *argv[])
 	insertar(&raiz,22);
 	insertar(&raiz,56);
 	insertar(&raiz,93);
-	insertar(&raiz,135);
+	insertar(&raiz,135);*/
 /*
 	insertar(&raiz,60);
 	insertar(&raiz,40);
@@ -102,6 +106,15 @@ int main(int argc, char const *argv[])
 	insertar(&raiz,50);
 	insertar(&raiz,68);*/
 
+	insertar_balanceado(&raiz,65);
+	insertar_balanceado(&raiz,50);
+	insertar_balanceado(&raiz,23);
+	insertar_balanceado(&raiz,70);
+	insertar_balanceado(&raiz,82);
+	insertar_balanceado(&raiz,68);
+	insertar_balanceado(&raiz,39);
+
+	printf("\n");
 	ver_Arbol(raiz,0,0);
 
 	h=altura_arbol(raiz);
@@ -124,7 +137,7 @@ nodoB* crearNodo(int d){
 	return newe;	
 }
 ///////////INSERTAR NODO A ARBOL BINARIO////////////////////////
-int insertar(nodoB **axraiz,int d){
+/*int insertar(nodoB **axraiz,int d){
 	if(!(*axraiz)){
 		(*axraiz)=crearNodo(d);
 		return 1;
@@ -135,6 +148,33 @@ int insertar(nodoB **axraiz,int d){
 		if((*axraiz)->dato==d) return 2;
 		return insertar(&(*axraiz)->izq,d);
 	}	
+}*/
+int insertar_balanceado(nodoB **axraiz,int d)
+{
+	if(!(*axraiz)){
+		(*axraiz)=crearNodo(d);
+		return 1;
+	}
+	if(d>(*axraiz)->dato){
+	    insertar_balanceado(&(*axraiz)->der,d);
+	}else{
+		if((*axraiz)->dato==d) return 2;
+		insertar_balanceado(&(*axraiz)->izq,d); 
+	}
+	if(equilibrio_arbol((*axraiz))>=2)
+	{
+		if(equilibrio_arbol((*axraiz)->der)==-1)
+			balanceo_DI(axraiz);
+		else
+			balanceo_DD(axraiz);
+	}
+	if(equilibrio_arbol((*axraiz))<=-2)
+	{
+		if(equilibrio_arbol((*axraiz)->izq)==1)
+			balanceo_ID(axraiz);
+		else
+		    balanceo_II(axraiz);
+	}
 }
 //////////Imprime arbol//////////////
 void ver_Arbol(nodoB *raiz,int c,int i){
@@ -243,4 +283,51 @@ int equilibrio_arbol(nodoB *raiz)
 	h1=altura_arbol(raiz->izq);
 	h2=altura_arbol(raiz->der);
 	return h2-h1;
+}
+int balanceo_DD(nodoB **raiz)
+{
+	if(!(*raiz))
+		return 0;
+	nodoB *aux=(*raiz)->der;
+	(*raiz)->der=aux->izq;
+	aux->izq=*raiz;
+	*raiz=aux;
+	return 1;
+}
+int balanceo_II(nodoB **raiz)
+{
+	if(!(*raiz))
+		return 0;
+	nodoB *aux=(*raiz)->izq;
+	(*raiz)->izq=aux->der;
+	aux->der=(*raiz);
+	*raiz=aux;
+	return 1;
+
+}
+int balanceo_DI(nodoB **raiz)
+{
+	if(!(*raiz))
+		return 0;
+	nodoB *aux1=(*raiz)->der;
+	nodoB *aux2=aux1->izq;
+	aux1->izq=aux2->der;
+	aux2->der=aux1;
+	(*raiz)->der=aux2->izq;
+	aux2->izq=(*raiz);
+	(*raiz)=aux2;
+	return 1;
+}
+int balanceo_ID(nodoB **raiz)
+{
+	if(!(*raiz))
+		return 0;
+	nodoB *aux1=(*raiz)->izq;
+	nodoB *aux2=aux1->der;
+	aux1->der=aux2->izq;
+	aux2->izq=aux1;
+	(*raiz)->izq=aux2->der;
+	aux2->der=(*raiz);
+	(*raiz)=aux2;
+	return 1;
 }

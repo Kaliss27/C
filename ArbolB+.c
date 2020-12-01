f329a0d1575c687d331351de6cb68f794bedf7a3
Karen Peralta <56215336+Kaliss27@users.noreply.github.com>
2020-12-01 03:24:38 -0600

    Update ArbolB+.c
    Construccion de funciones para partir las paginas

--- ArbolB+.c
+++ ArbolB+.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define D 2 
+
 typedef struct datos_almacenados   //Estructura que almacenara los datos pertenecientes a cada clave
 {
 	int id;
@@ -29,6 +31,7 @@ page *crear_pagina(int d);
 clave *crear_clave(int d);
 void ver_claves_en_pag(clave *inicio);
 void insertar_claves_en_pag(clave **raiz_p,int d);
+int calcular_m(clave *raiz);
 
 int main(int argc, char const *argv[])
 {
@@ -64,6 +67,15 @@ clave *crear_clave(int d) //Aloja espacio de memoria para almacenar las claves a
 	return newe;
 }
 
+clave ** clave_centro(clave **raiz, int c)
+{
+    if (!raiz)
+        return NULL;
+    if (cont == D)
+        return raiz;
+    return clave_centro(&(*raiz)->sig,c+1);
+}
+
 page *crear_pagina(int d) // Aloja espacio de memoria para cada pagina
 {
 	page *newe=(page*)malloc(sizeof(page));
@@ -72,6 +84,21 @@ page *crear_pagina(int d) // Aloja espacio de memoria para cada pagina
 	newe->inicio=crear_clave(d);
 	newe->ant=NULL;
 	return newe;
+}
+page **crear_pagina_p( ) // Aloja espacio de memoria para cada pagina
+{
+	page *newe=(page*)malloc(sizeof(page));
+	if(!newe)
+		return NULL;
+	newe->inicio=NULL;
+	newe->ant=NULL;
+	return newe;
+}
+
+int calcular_m(clave *raiz) // Cuenta los elementos alojados en una pagina
+{
+	if (!raiz) return 0;
+    return 1 + calcular_m(raiz->sig);
 }
 
 void insertar_claves_en_pag(clave **raiz_p,int d) // Interta claves en una pagina
@@ -83,6 +110,17 @@ void insertar_claves_en_pag(clave **raiz_p,int d) // Interta claves en una pagin
 	(*raiz_p)=newe;
 	return;
 }
+
+void modificar_paginas(clave **centro,pagina **ant,pagina **abajo) // Partir pagina...
+{
+	(*centro)->datos_alm->= izq;
+    (*centro)->dato->der = der;
+    izq->inicio = (*raiz)->inicio;
+    der->inicio = (*centro)->sig;
+    (*raiz)->inicio = *centro;
+    (*centro)->sig = NULL;
+    (*centro) = NULL;
+}
 int insercion(page **raiz,int id) // Operacion general de insercion
 {
 	if(!(*raiz))
@@ -92,9 +130,15 @@ int insercion(page **raiz,int id) // Operacion general de insercion
 			return 1;
 	}
 	insertar_claves_en_pag(&(*raiz)->inicio,id);
-	/*clave *newe=crear_clave(id);
-	newe->sig=(*raiz)->inicio;
-	(*raiz)->inicio=newe;*/
+	if(calcular_m((*raiz)->inicio) > 2*D)
+	{
+		clave **centro=clave_centro(&(*raiz)->inicio,0);
+		pagina **n_ant=crear_pagina_p();
+		pagina **n_abajo=crear_pagina_p();
+		if(!(n_ant || n_abajo))
+			return 0;
+		modificar_paginas(centro,n_ant,n_abajo);
+	}
 	return 1;
 
 }
